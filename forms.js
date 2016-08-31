08a7a84bb76f78a9edbfcb15579ab0a628fd68e0
Gleb Scherban <posijuur@mail.ru>
2016-08-31 23:30:09 +0300

    top

--- forms.js
+++ forms.js
@@ -1,5 +1,20 @@
 ;(function () {
 	var hiddenElement = null;
+	$('.foms_input').focus(function(event) {
+		event.preventDefault();
+		event.stopPropagation();
+		var targetParent = null;	
+		targetParent = $(this).parent();
+		$('label + div').hide();
+		$('+ div', targetParent).show();
+	});
+	$('button').focus(function(event) {
+		$('label + div').hide();
+	});;
+	$('.foms_input').click(function(event) {
+		event.preventDefault();
+		event.stopPropagation();
+	});
 	$('.foms_input').hover(function() {
 		var targetParent = null;	
 		targetParent = $(this).parent();
@@ -9,13 +24,23 @@
 	});
 	$('button').on('click', function(event) {
 		event.preventDefault();
+		event.stopPropagation();
 		var target = event.target;
 		$('.hiddenHelp').show();
 	});;
 	$('label').on('click', function(event) {
 		event.preventDefault();
+		event.stopPropagation();
 		var target = event.target;
 		$('label + div').hide();
 		$('+ div', target).show();
+	});
+	$('input').mouseenter(function(event) {
+		event.preventDefault();
+		event.stopPropagation();
+		var target = event.target;
+		var targetParent = null;	
+		targetParent = $(this).parent();
+		$('+ div', targetParent).show();
 	});
 })();
